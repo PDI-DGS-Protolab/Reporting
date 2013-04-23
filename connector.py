f403e2d65c8fabc9b912dcae73be2acf48c053b3
Carlos David Rodríguez Peña <cdavidrp@gmail.com>
2013-04-23 13:28:35 +0200

    Finnish pie chart generation

--- connector.py
+++ connector.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import csv
+import json
 
 from jasperserver.rest import Client
 from jasperserver.admin import User, Role
@@ -9,40 +10,100 @@ from jasperserver.synchronization import SyncResources
 from jasperserver.services import Report, Resource, Resources
 from jasperserver.exceptions import *
 
+
+URL = 'http://ec2-54-228-152-90.eu-west-1.compute.amazonaws.com:8080/jasperserver-pro'
+USER = 'jasperadmin'
+PASSWORD = 'jasperadmin'
+DEFAULT_COLOURS = [ "FFFF10AA", "FFAA0AAA", "FF5505AA", "FF0000AA" ]
+
+def connect():
+    try:
+        client = Client( URL, USER, PASSWORD )
+
+    except JsException:
+        print 'Error Authentification FAIL!'
+
+    return client
+
+def downloadReport( location, name, format ):
+    report = Report(client, location )
+    res = report.run( name, output_format = format )
+    return res
+
+
 def parseCSV( csv ):
     data = []
     lines = csv.split( "\n" )
-    keys = lines [ 0 ].split(',')
+    keys = lines [ 0 ].split( ',' )
+
     for l in lines:
-        values = l.split(',')
+        values = l.split( ',' )
         d = {}
-        for k,v in zip(keys, values):
+        for k,v in zip( keys, values ):
             d[k] = v
-        data.append(d)
-    del data[0]
-    del data[len(data) -1]
+        data.append( d )
+
+    # The next two lines are dependant on the report we are using
+    del data[0] # This line takes out the keys line from the data set
+    del data[len(data) -1] # This line takes out a blank data line at the end
     return data
 
 def filterbyKey( key, data ):
     res = []
     for d in data:
         if key in d:
-            res.append(d[key])
+            res.append( d[ key ] )
     return res
 
+def createPieChart( labelKey, valueKey, data, colours = DEFAULT_COLOURS ):
+    labels = filterbyKey( labelKey, data )
+    values = filterbyKey( valueKey, data )
+
+    data = []
+    for i in range( 0, len( labels ) ):
+        d = {}
+        d['value']  = values[ i ]
+        d['label']  = labels[ i ]
+        d['colour'] = colours[ i % ( len( colours ) ) ]
+        data.append( d )
+
+    item = { 'item' : data }
+    return item
+
 if __name__ == '__main__':
-    try:
-        client = Client('http://ec2-54-228-152-90.eu-west-1.compute.amazonaws.com:8080/jasperserver-pro', 'jasperadmin', 'jasperadmin')
+    client = connect()
+    # users = User(client).search()
+    # role = Role(client).search()
+    csv_rep = downloadReport( '/reports', 'Bundles', 'csv' )
+    data = parseCSV( csv_rep )
+    res = createPieChart( '_id', 'expirat', data )
+    json = json.dumps(res)
+    print json
 
-    except JsException:
-        print 'Error Authentification FAIL!'
 
-    users = User(client).search()
-    role = Role(client).search()
-
-    report = Report(client, '/reports')
-    csv_rep = report.run('Bundles', output_format='csv')
-    
-    data = parseCSV(csv_rep)
-    res = filterbyKey('_id', data)
-    print res
+"""
+    { 
+"item": [ 
+{ 
+"value": "100", 
+"label": "May", 
+"colour": "FFFF10AA" 
+}, 
+{ 
+"value": "160", 
+"label": "June", 
+"colour": "FFAA0AAA" 
+}, 
+{ 
+"value": "300", 
+"label": "July", 
+"colour": "FF5505AA" 
+}, 
+{ 
+"value": "140", 
+"label": "August", 
+"colour": "FF0000AA" 
+} 
+] 
+}
+"""
