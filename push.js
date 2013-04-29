6618db4b5e4b743895da5bda9aea988a79acc03d
Alberto Blázquez Rodríguez <hola@albertoblazquez.net>
2013-04-29 10:28:56 +0200

    Push working

--- push.js
+++ push.js
@@ -2,35 +2,38 @@
 var Geckoboard = require('geckoboard-push');
 
 var API_KEY = '180911140d66035ce434390cf0dac36f';
-var conn = new Geckoboard({ api_key : API_KEY });
+var PIE_KEY = '31473-8a9b8503-c75d-483a-8b9f-d1324c5d01fc';
+
+var gecko = new Geckoboard({ api_key : API_KEY });
 
 var items = [
-  {
-    value: "100",
-    label: "May",
-    colour: "FFFF10AA"
-  },
-  {
-    value: "160",
-    label: "June",
-    colour: "FFAA0AAA"
-  },
-  {
-    value: "300",
-    label: "July",
-    colour: "FF5505AA"
-  },
-  {
-    value: "140",
-    label: "August",
-    colour: "FF0000AA"
-  }
+    {
+        value: "100",
+        label: "A1",
+        colour: "FFFF10AA"
+    },
+    {
+        value: "160",
+        label: "A2",
+        colour: "FFAA0AAA"
+    },
+    {
+        value: "300",
+        label: "A3",
+        colour: "FF5505AA"
+    },
+    {
+        value: "140",
+        label: "A4",
+        colour: "FF0000AA"
+    }
 ];
 
-var json = JSON.stringify(items);
-
-var pie = foo.pie('pie_widget_key');
-
-pie.send(json, function(err, response){
-  //callback with response from geckoboard
+var pie = gecko.pie(PIE_KEY);
+pie.send(items, function(err, response){
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(response);
+    }
 })
