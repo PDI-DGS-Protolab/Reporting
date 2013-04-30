1ad3280e8186406015a8f805b866db2801c9cb0c
Salvador Perez Martin <sisekeom@gmail.com>
2013-04-30 13:19:53 +0200

    Add Custom push functionality

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
-var module_db = require('db')
+var db = require('./db.js');
+var push = require('./push.js');
 
 var options = {
     url    : 'ec2-54-228-152-90.eu-west-1.compute.amazonaws.com',
@@ -7,12 +8,29 @@ var options = {
     dbname : 'day-10'
 };
 
-var db = var module_db.startClient(options);
+db.startClient(options);
 
-var keys = ['category'];
+var key = 'category';
 var col = 'accounts';
 
-var cats = db.getData(keys, col);
-cats.forEach(function(c) {
+var purchase = 0;
+var gift = 0;
+var prepaid = 0;
+var def = 0;
 
+var cats = db.getData(key, col, function(err, response) {
+	response.forEach(function(c) {
+		if (c == "PURCHASE"){
+			purchase = purchase + 1;
+		} else if(c == "GIFT"){
+			gift = gift + 1;
+		}else if (c == "PREPAID"){
+			prepaid = prepaid + 1;
+		}else{
+			def = def + 1;
+		}
+	});
 });
+
+items = [def, purchase, gift, prepaid];
+push.makePie(items,'31473-8a9b8503-c75d-483a-8b9f-d1324c5d01fc');
