0892034173c3179840cd1bb8e6f94f03aa493729
Carlos David Rodríguez Peña <cdavidrp@gmail.com>
2013-04-30 10:17:26 +0200

    Move database logic to another file

--- db.js
+++ db.js
@@ -1,3 +1,16 @@
 // conectar
 
 // extraer datos de una col concreta
+var Mongodb = require('mongodb');
+var server = new Mongodb.Server('ec2-54-228-152-90.eu-west-1.compute.amazonaws.com', 27017, {});
+var client = new Mongodb.Db('day-10', server);
+
+var listAllData = function(err, collection) {
+    collection.find().toArray(function(err, results) {
+        console.log(results);
+    });
+}
+
+client.open( function( err, pClient ) {
+    client.collection( 'accounts', listAllData );
+});
