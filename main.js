e0199199dd8e2f426eb6a256933acf0aafe2b0d2
Alberto Blázquez Rodríguez <hola@albertoblazquez.net>
2013-05-03 11:43:33 +0200

    updating files

--- main.js
+++ main.js
@@ -27,23 +27,72 @@ var filterCategories = function(response) {
     push.makePie(items,'31473-8a9b8503-c75d-483a-8b9f-d1324c5d01fc');
 };
 
+
 var createPie = function(key, col, cb) {
     db.getData(key, col, function(err, response) {
         cb(response);
     });
 };
 
-var filterPrice = function(response) {
-    var items = [];
-    var key = 'price';
 
+function numElements(arr) {
+    var a = [], b = [], prev;
+
+    arr.sort();
+    for ( var i = 0; i < arr.length; i++ ) {
+        if ( arr[i] !== prev ) {
+            a.push(arr[i]);
+            b.push(1);
+        } else {
+            b[b.length-1]++;
+        }
+        prev = arr[i];
+    }
+
+    return [a, b];
+}
+
+var countData = function( data ) {
+    var items = {};
+    for ( var d in data ) {
+        for ( var p in data[d] ){
+            items[ data[d][p] ] = items[ data[d][p] ] ? items[ data[d][p] ] + 1 : 1;
+        }
+    }
+    return items;
+};
+
+var filterPrice = function(response) {
+    var raw_items = [];
+    var key = 'price';
+/*
     response.forEach(function(c) {
-        items.push( c[key] );
+        raw_items.push( c[key] );
     });
 
+    var elements = numElements(raw_items);
+    var times    = elements[0];
+    var numbers  = elements[1];
+
+    var filtered_items = [];
+
+    for (var i=0, len=times.length; i < len; i++) {
+        filtered_items.push({
+            value : numbers[i],
+            text  : JSON.stringify( times[i] )
+        });
+    };
+*/
+
+    var counts = countData(response);
+    for (var c in counts) {
+        console.log(c +" - " + counts[c]);
+        con
+    }
     var RAG_COLUMNS_KEY = '31473-7653d56b-a456-4512-9343-657404b55700';
-    push.makeRagColumns(items, RAG_COLUMNS_KEY);
+    push.makeRagColumns(filtered_items, RAG_COLUMNS_KEY);
 };
+
 
 var createRagColumns = function(key, col, cb) {
     db.getData(key, col, function(err, response) {
--- push.js
+++ push.js
@@ -44,6 +44,7 @@ var makePie = function(items, key) {
             colour: "FF0000AA"
         }
     ];
+    console.log(pie_items);
     pie.send(pie_items, callback);
 };
 
@@ -74,51 +75,17 @@ var line = function() {
 */
 
 
-function numElementsArray(arr) {
-    var a = [], b = [], prev;
-
-    arr.sort();
-    for ( var i = 0; i < arr.length; i++ ) {
-        if ( arr[i] !== prev ) {
-            a.push(arr[i]);
-            b.push(1);
-        } else {
-            b[b.length-1]++;
-        }
-        prev = arr[i];
-    }
-
-    return [a, b];
-}
-
-
 var makeRagColumns = function(items, KEY) {
     var ragcol = gecko.ragColumn(KEY);
-    var ragcol_items = [];
-
-    /*
-    items.forEach(function(it) {
-        ragcol_items.push({
-            value: it,
-            text: it//JSON.stringify(it)
-        });
-    });
-    */
-
-    var itemsCounted = numElementsArray(items);
-    var times = itemsCounted[0];
-    var numbers = itemsCounted[1];
-
-    for (var i=0, len=times.length; i < len; i++) {
-        ragcol_items.push({
-            value: times[i],
-            text: numbers[i]
-        });
-    };
-
     var ragcol_type = 'reverse';
-    console.log(ragcol_items);
-    //ragcol.send(ragcol_items, ragcol_type, callback);
+
+    var LIMIT = 3;    // Limit of data elements
+    for (var i = LIMIT, len = items.length; i < len; i++) {
+        items.pop();
+    }
+
+    //console.log(items);
+    ragcol.send(items, ragcol_type, callback);
 };
 
 
