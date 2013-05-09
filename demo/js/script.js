8ba158e28c33e0f140b30214995f9400c30f44b6
Alberto Blázquez Rodríguez <hola@albertoblazquez.net>
2013-05-09 12:15:45 +0200

    Code refactored and first report uploaded

--- demo/js/script.js
+++ demo/js/script.js
@@ -1,37 +1,26 @@
-//$(function() {
-    var hidden = 'hidden';
+$(function() {
 
+    var hidden = 'hidden';
     var panels = $('.panel');
 
-    var sheet  = $('.sheet');
-    var sheet2 = $('.sheet2');
-    var stats  = $('.stats');
-
-
-    var showStats = function () {
+    var displayPanel = function (panel) {
         panels.addClass(hidden);
-        panels.filter('.gecko').removeClass(hidden);
-    };
-
-    var showDashboard = function () {
-        panels.addClass(hidden);
-        panels.filter('.dashboard').removeClass(hidden);
-    };
-
-    var showTable = function () {
-        panels.addClass(hidden);
-        panels.filter('.table').removeClass(hidden);
+        panels.filter(panel).removeClass(hidden);
     };
 
 
     // Main
     (function () {
 
-        sheet.on('click', showTable);
-        sheet2.on('click', showDashboard);
-        stats.on('click', showStats);
+        [ '.table', '.report', '.dashboard', '.gecko' ].forEach(function(elem) {
+            $(elem).on('click', function() {
+                displayPanel(elem);
+            });
+        });
+
+        // Gecko panel will be shown first
         panels.addClass(hidden).filter('.gecko').removeClass(hidden);
 
     })();
 
-//});
+});
